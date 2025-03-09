fd279874eb026f8a2bce1916924ec5ac6f6cf4f4
nastya24704 <nastazeravina@gmail.com>
2025-03-09 15:06:56 +0700

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,9 @@
+print('Какие два слова передал первой радиограммой Александр Попов:')
+print('Первое слово:')
+firstword=input()
+print('Второе слово:')
+secondword=input()
+if firstword=='Генрих' and secondword=='Герц':
+    print('Верно')
+else:
+    print('Неверно')
